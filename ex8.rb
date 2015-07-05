11be7c08cc7b5d486ab087f9b7abca3e2dec462c
sreil <seanpreilly@gmail.com>
2015-07-05 12:15:57 -0600

    commented

--- ex8.rb
+++ ex8.rb
@@ -1,8 +1,13 @@
+#formatter string
 formatter = "%{first} %{second} %{third} %{fourth}"
 
+#will print 1, 2, 3, 4
 puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+#will print one, two, three, four
 puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+#will print true, false, true, false
 puts formatter % {first: true, second: false, third: true, fourth: false}
+#will print original string x4
 puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
 
 puts formatter % {
