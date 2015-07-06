94b67b623f592ff373095eb2c73bfbd3e79d3801
sreil <seanpreilly@gmail.com>
2015-07-06 14:05:47 -0600

    added ex13

---
+++ ex13.rb
@@ -0,0 +1,9 @@
+first, second, third = ARGV
+
+puts "Enter 3 variables"
+first = gets.chomp
+second = gets.chomp
+third = gets.chomp
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
